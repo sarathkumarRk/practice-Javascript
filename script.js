e4eac8c56c1f386b6826f144633d47100e6c2b55
Sarathkumar R <167192152+sarathkumarRk@users.noreply.github.com>
2025-07-24 13:21:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-document.getElementById('startbtn').addEventListener('click', function(){
-    document.getElementById('results').innerHTML = ``
+let weathervalue = 'sarath';
 
-    let totallap = parseInt(document.getElementById('input').value)
-    let currentlap = 1;
-
-    while (currentlap <= totallap) {
-        document.getElementById('results').innerHTML += `<h1>lap${currentlap} is completed</h1>`
-        currentlap++;
-        
-    }
-    console.log('you won');
-})
+if (weathervalue === 'israning'|| weathervalue === 'hot') {
+    document.getElementById('results').innerHTML = 'Take in umbralla'  
+}else if(weathervalue === 'sunny'){
+    document.getElementById('results').innerHTML = 'Take the cooling glass'
+}else{
+    document.getElementById('results').innerHTML = 'invalid item'
+}
